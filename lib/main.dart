88637c6e1c9e9041f783f6cc1b5faba559574172
Yuji Ono <yuji.ono@woven-planet.global>
2021-10-14 15:29:30 +0900

    please check,nagai

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(const MyApp());
 }
-
+//hello nagai can you see?
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
