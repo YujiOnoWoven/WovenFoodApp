b9f4abe50f8e24ef769d3432111fb04325629b18
Yuji Ono <yuji.ono@woven-planet.global>
2021-10-29 15:09:22 +0900

    no message

--- lib/pages/cook_screen.dart
+++ lib/pages/cook_screen.dart
@@ -53,7 +53,6 @@ class CookScreen extends StatelessWidget {
                               ],
                             ),
                           ),
-
                       ],
                     ),
                     Column(
